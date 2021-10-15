bea48e3c59b756bfd2e9ad6d9bf29a5351ef39f3
danghuytu12 <danghuytu.thd2006@gmail.com>
2021-10-15 11:13:29 +0700

    add rightside home

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,11 +4,28 @@ import React from 'react'
 const Main = () => {
     return (
         <Container>
-b
+            <ShareBox>Share</ShareBox>
         </Container>
     )
 }
 const Container = styled.div`
     grid-area: main;
+`
+const CommonCard=styled.div`
+    text-align: center;
+    overflow: hidden;
+    margin-bottom: 8px;
+    background-color: #fff;
+    border-radius:5px;
+    position: relative;
+    border: none;
+
+`
+const ShareBox = styled(CommonCard)`
+    display: flex;
+    flex-direction: column;
+    color: #958b7b;
+    margin: 0 0 8px;
+    background: white;
 `
 export default Main;
